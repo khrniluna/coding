5a4338c7d5c3438df969efd733b9e0edac0e3b13
khrniluna <khayranilna@gmail.com>
2024-06-09 19:44:57 +0700

    about.lunaa

--- about you.py
+++ about you.py
@@ -18,15 +18,15 @@ def sing_lyric(lyric, delay, speed):
 
 def sing_song():
     lyrics = [
-        ("Do you think I have forgotten?", 0.1),
-        ("Do you think I have forgotten?", 0.1),
-        ("Do you think I have forgotten", 0.1),
-        ("about you?", 0.2),
-        ("There was something bout you that now I cant remember", 0.08),
-        ("Its the same damn thing that made my heart surrender", 0.1),
-        ("And I miss you on a train, I miss you in the morning", 0.1),
-        ("I never know what to think about", 0.1),
-        ("I think about youuuuuuuuuuuuuuuuuuuuuuuuuuu", 0.1)
+        ("tuhan tolong yakinkan diriku", 0.1),
+        ("bahwa dia bukan lah untukku?", 0.1),
+        ("tolong hilangkan perasaan ku", 0.1),
+        ("agar ku tak lagi memikirirkan dia", 0.2),
+        ("telah ku coba melupakan diri mu", 0.08),
+        ("namun apa daya hati ini sulit terima", 0.1),
+        ("tuhan tolong yakinkan diriku", 0.1),
+        ("bhawa dia bukan lah untukku", 0.1),
+        ("tolong hilangkan perasaanku ", 0.1)
     ]
     delays = [0.3, 5.0, 10.0, 15.0, 20.3, 25.0, 27.0, 30.2, 33.3]
     
